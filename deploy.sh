d9550d89d19e228d4b532b3207b2fd831e55cb85
Matias-S1lva <matiasleandro752@gmail.com>
2022-08-16 23:07:12 -0300

    first commit

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:Matias-S1lva/rick-and-morty.git main:gh-pages
 
 cd -
